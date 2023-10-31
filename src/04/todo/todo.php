53409f7ea606db841695e525a0467ae14fc30836
Yusei Mizutani <yusei4520030401@gmail.com>
2023-10-31 19:05:55 +0900

    todo

--- src/04/todo/todo.php
+++ src/04/todo/todo.php
@@ -13,10 +13,27 @@ $todo_over_list = array();
 $todo_upcoming_list = array();
 $today_date = date("Y/m/d");
 
+$str  = "
+2023/10/21	アシアル全体のミーティング\n
+2023/10/22	海原・森川とミーティング\n
+2023/11/1	日経ソフトウエア原稿締め切り\n
+2023/11/1	PHPカンファレンス\n
+2023/10/30	アシアル全体のミーティング\n
+2023/11/1	駅伝大会\n
+2023/11/10	書類の提出期限\n
+2024/3/1	求人票解禁\n
+";
+
+// $file_contents = preg_split("\n", $str);
+// echo($file_contents);
+
 foreach ($file_contents as $line) {
-    $line = mb_convert_encoding($line, "UTF-8", "sjis"); // UTF-8に変換
-    list($todo_date_str, $today_title) = explode("\t", $line); // タブで区切る
+    $line = mb_convert_encoding($line, "UTF-8", "utf-8,sjis"); // UTF-8に変換
+    list($todo_date_str, $todo_title) = explode(" ", $line); // タブで区切る
+    echo ($todo_title);
+    
     $todo_date = date("Y/m/d", strtotime($todo_date_str));
+    // echo ($todo_date_str);
 
     if ($todo_date < $today_date) {
         $todo_over_list[] = array("title" => $todo_title, "date" => $todo_date);
@@ -24,6 +41,12 @@ foreach ($file_contents as $line) {
         $todo_upcoming_list[] = array("title" => $todo_title, "date" => $todo_date);
     }
 }
+
+// foreach ($todo_over_list as $task) {
+//     echo "Title: " . $task['title'] . "\n";
+//     echo "Date: " . $task['date'] . "\n";
+//     echo "\n"; // タスク間を区切るための改行
+// }
 
 require_once("smarty/Smarty.class.php");
 $smarty = new Smarty();
